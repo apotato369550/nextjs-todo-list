beb5186663922b9943e6fc38cf07c66ffb36a094
John Andre Yap <johnandresyap510@gmail.com>
2025-07-21 12:08:50 +0800

    Worked on id routes

--- src/app/api/todos/[id]/route.js
+++ src/app/api/todos/[id]/route.js
@@ -4,36 +4,97 @@ export async function GET(_, { params }) {
     const todos = getTodos();
     const todoId = params.id;
     // find todo using filtering
+    const todo = todos.find(t => t.id === todoId);
 
     // if todo not found, return a bad response object 404
+    if (!todo) {
+        return new Response({ error: "Todo not found..." }, { status: 404 })
+    }
+
+
     // otherwise, return 200
+    return new Response(
+        JSON.stringify(todo),
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        }
+    );
 }
 
 export async function DELETE(_, { params }) {
     const todos = getTodos();
     const todoId = params.id;
     // find todo using loops/filtering
+    const filtered = todos.filter(t => t.id !== todoId);
 
     // check if the filtered todos length == the current todos length
-    // if they're equal, todo does not exist, return 494
-    // save the filtered todos
-    // if failed to save, failed to delete, return 500
+    if (filtered.length === todos.length) {
+        // if they're equal, todo does not exist, return 404
+        return new Response(
+            { error: "Todo not found..." },
+            { status: 404 }
+        );
+    }
 
+    // save the filtered todos
+    const success = saveTodos(filtered);
+    if (!success) {
+        // if failed to save, failed to delete, return 500
+        return new Response(
+            { error: "Failed to delete todo..." },
+            { status: 500 }
+        );
+
+    } 
     // otherwise, all is well. return null object + status 204
+    return new Response(
+        null,
+        { status: 204 }
+    );
 }
 
-export async function PUT(response, { params }) {
+export async function PUT(request, { params }) {
     // get body using await
+    const body = await request.json();
     // get todos using getTodos()
+    const todos = getTodos();
     // get index using findIndex with condition
+    const todoIndex = todos.findIndex(t => t.id === params.id);
 
     // if index == -1, return a new response object 404 todo not found
+    if (index === -1) {
+        return new Response(
+            { error: "Todo not found..." },
+            { status: 404 }
+        );
+    }
 
     // create an updated todo using ellipses
+    const updatedTodo = {
+        ...todos[todoIndex],
+        ...body // this part overrides any fields...
+    }
 
     // change todo using index
+    todos[todoIndex] = updatedTodo;
+
     // save todo store in success variable
+    const success = saveTodos(todos);
+    
     // if not successfully saved, failed to update todo, return 500
+    if (!success) {
+        // if failed to save, failed to delete, return 500
+        return new Response(
+            { error: "Failed to update todo..." },
+            { status: 500 }
+        );
+
+    } 
 
     // otherwise, all is well, return updated todo and status 200
+    return new Response(
+        JSON.stringify(updatedTodo),
+        { status: 200 }
+    );
 }
