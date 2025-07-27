c40b1fdb55bdffa8a11061beda1ce5192ff61467
John Andre Yap <johnandresyap510@gmail.com>
2025-07-27 13:21:17 +0800

    Installed axios and worked on todo details page

--- src/components/TodoDetails.jsx
+++ src/components/TodoDetails.jsx
@@ -1,28 +1,50 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import TodoList from './TodoList'
 
 const TodoDetails = ({ id }) => {
+  /*
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [completed, setCompleted] = useState("");
   const [deleted, setDeleted] = useState("");
+  */
+  const [todo, setTodo] = useState({})
 
-  function getTask() {
-    axios.get("/api/todos/" + id)
-      .then(response => {
-        console.log(response);
-
-      })
+  async function getTask() {
+    try {
+      const response = await axios.get(`/api/todos/${id}`);
+      console.log(response);
+      setTodo(response.data);
+    } catch (error) {
+      console.error('Error fetching todo:', error);
+    }
   }
+
+  function deleteTask(id) {
+    return;
+  }
+
+  function completeTask(id) {
+    return;
+  }
+
+  useEffect(() => {
+    getTask();
+  }, [])
 
   return (
     <div>
       <h1>{todo.title}</h1>
+      <p>{todo.description}</p>
       <button onClick={() => deleteTask(todo.id)}>X</button>
-      <Link href={"/todos/" + todo.id}>
-        <button>View Todo</button>
+      <button onClick={() => completeTask(todo.id)}>Complete Task</button>
+      <Link href={"../"}>
+        <button>Go back to main page</button>
       </Link>
     </div>
   )
